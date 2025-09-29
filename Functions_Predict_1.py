086e7ca554583f9fb5013cf4329c2cc0dd75d4c6
Trevor Swarm <tswarm@lccc.wy.edu>
2025-09-29 14:42:34 -0600

    Create Functions_Predict_1.py

---
+++ Functions_Predict_1.py
@@ -0,0 +1,19 @@
+"""
+Filename: Functions_Predict_1.py
+
+Instructions:
+Read the code below. Without running it, predict what the output will be in the
+terminal. Write down your prediction and your reasoning.
+Then, run the code to see if you were right!
+"""
+
+# Define a function to print a message
+def show_message():
+    print("This message is from inside the function.")
+
+# Main part of the program
+print("This is the first line of the program.")
+
+show_message() # This is the function call
+
+print("This is the last line of the program.")
